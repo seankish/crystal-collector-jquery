92e46d956aec40cd0d6795ddd822c7c576a4bf49
skish1987 <seankish1@gmail.com>
2019-05-30 09:52:29 -0700

    Working game

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,11 +4,11 @@ var losses = 0
 var targetScore;
 var pinkValue; 
 var blueValue;
-
 var greenValue;
 var yellowValue;
-var playerScore =
+var playerScore;
 reset();
+
 // randomize crystal and targetscore after each win/loss
 function reset() {
      playerScore = 0;
@@ -17,16 +17,46 @@ function reset() {
      greenValue = Math.floor(Math.random() * 12)+1;
      yellowValue = Math.floor(Math.random() * 12)+1;
      targetScore = Math.floor(Math.random() * 102)+ 19;
-    console.log (targetScore);
-    console.log(yellowValue)
+     $("#target-score").html("Try to match: " + targetScore)
+     console.log(targetScore)
 }
-
+// Turns images into buttons and prints the random crystal value to  var playerscore
 $("#pink").on("click", function() {
-     playerScore= this.playerScore + pinkValue;
-     console.log(playerScore)
-  
-
+    playerScore = playerScore + pinkValue; 
+     $("#player-score").text(playerScore)
 })
+$("#blue").on("click", function() {
+    playerScore = playerScore + blueValue; 
+     $("#player-score").text(playerScore)
+})
+$("#green").on("click", function() {
+    playerScore = playerScore + greenValue; 
+     $("#player-score").text(playerScore)
+})
+$("#yellow").on("click", function() {
+    playerScore = playerScore + yellowValue; 
+     $("#player-score").text(playerScore)
+})
+// Track win/losses and reset random values after each round
+document.addEventListener("click", function(){
+if (playerScore === targetScore) {
+    wins++;
+    $("#wins-text").text("Wins: " + wins);
+    $("#player-score").text(" ");
+    reset();
+    alert("Hooray! You won!")
+}
+else if (playerScore > targetScore) {
+    losses++;
+    $("#losses-text").text("Losses: " + losses)
+    $("#player-score").text(" ");
+    reset();
+    alert( "Sorry, You lost");
+}
+})
+
+    
+
 
 
 
